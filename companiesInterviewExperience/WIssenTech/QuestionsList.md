6937ebf305a4f121fe95d058de3fabe39f394a57
sudoshubh <shubhamsharmavks@gmail.com>
2026-05-12 01:25:26 +0530

    solution2

---
+++ companiesInterviewExperience/WIssenTech/QuestionsList.md
@@ -0,0 +1,10 @@
+- Wissen technology
+
+- Q. Rain Water Trapping: A popular, challenging array problem.
+
+
+- Q. Longest Substring Without Repeating Characters: Sliding window technique.
+- Q. Flatten Nested Array/List: E.g., {{1}, 2, {3, 4}} to {1, 2, 3, 4}.
+- Q. Spiral Traversal of 2D Matrix: Navigating matrix elements in a spiral pattern.
+- Q. Find Kth Largest Element: Using Heaps or QuickSelect.
+- Q. Reverse/Sort Strings: String manipulation techniques
---
+++ companiesInterviewExperience/WIssenTech/longestSubstringnonrepeatingCharacter.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class solution {
+    public int lengthOfLongestSubString(String s) {
+        int left = 0;
+        int maxLen = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+
+    }
+}
